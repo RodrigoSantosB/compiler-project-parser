30b9c984f2aece5236c3b1b9e8156bd92f785c20
Enzo Bissoli <enzogurgel15@gmail.com>
2026-03-24 08:48:04 -0300

    feat: read hardcoded source and src a char

--- paper-examples/src/CParser.hs
+++ paper-examples/src/CParser.hs
@@ -1,10 +1,38 @@
-module CParser (someFunc) where
+module CParser (test) where
+
+import Prelude (IO, FilePath, String, (>>=), return, print, putStrLn, snd, ($))
 
 import Symbol (Parser, symbol)
 
--- Parsing 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Prelude (readFile, Eq)
+
+import Text.Parsec
+
+-- First we need to provide src code that's the entry point
+-- First we import the primitives
+-- Then we parse strings
+-- Then we parse Tokens
+-- Then we parse expressions in C such as
+-- structs, functions, if and else, for loops and comments!
+-- Then we finish
+
+-- this reads the src code for our toy c code
+-- for the moment we'll hardcode the path
+readSrc :: FilePath -> IO String
+readSrc _ =  readFile "./src/toy.c"
+
+
+
+
+main :: IO ()
+main = do
+	contents <- readSrc ""
+	let parsed = test 'a' contents
+	case parsed of
+		[] -> print "nothing to parse"
+		(first:_) -> print $ snd first
 
 test:: Eq s => s -> Parser s s
 test toparse = symbol toparse
+
+
