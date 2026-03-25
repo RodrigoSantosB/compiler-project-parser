587a94517e75d80a284c9562ddb77ad02c3ce7e8
Enzo Bissoli <enzogurgel15@gmail.com>
2026-03-24 21:33:51 -0300

    feat: wip parsec parser for a subset of C

--- paper-examples/src/CParser.hs
+++ paper-examples/src/CParser.hs
@@ -1,12 +1,17 @@
-module CParser (test) where
+module CParser (readSrc) where
 
 import Prelude (IO, FilePath, String, (>>=), return, print, putStrLn, snd, ($))
 
-import Symbol (Parser, symbol)
+import Prelude (readFile, Eq, Either)
 
-import Prelude (readFile, Eq)
+-- type Parser = (Parsec String () Identity), needs to also pass the return type
+-- type ParsecT = Stream type(input), user state(optional), monad and then return type
+-- the original signature Parser a a :: input output -> [([input],output)] would be
+-- ParsecT input () Identity output 
+import Text.Parsec.String (Parser, parseFromFile)
+import Text.Parsec (ParseError, letter, parse, many)
 
-import Text.Parsec
+-- we need to adapt our type to Parsec's one
 
 -- First we need to provide src code that's the entry point
 -- First we import the primitives
@@ -18,21 +23,30 @@ import Text.Parsec
 
 -- this reads the src code for our toy c code
 -- for the moment we'll hardcode the path
-readSrc :: FilePath -> IO String
-readSrc _ =  readFile "./src/toy.c"
+
+readSrc :: FilePath -> IO (Either ParseError String)
+readSrc _ =  readFile "./src/toy.c" >>= \content ->
+		    return $ parse (many letter) "./src/toy.c" content
+
+data GenTokenParser = {
+
+}
+
+readString :: Either ParseError Identifier
+
+-- Now we need to read strings and deal with any problems that might appear
 
 
 
-
-main :: IO ()
-main = do
-	contents <- readSrc ""
-	let parsed = test 'a' contents
-	case parsed of
-		[] -> print "nothing to parse"
-		(first:_) -> print $ snd first
-
-test:: Eq s => s -> Parser s s
-test toparse = symbol toparse
+-- main :: IO ()
+-- main = do
+-- 	contents <- readSrc ""
+-- 	let parsed = test 'a' contents
+-- 	case parsed of
+-- 		[] -> print "nothing to parse"
+-- 		(first:_) -> print $ snd first
+--
+-- test:: Eq s => s -> Parser s s
+-- test toparse = symbol toparse
 
 
