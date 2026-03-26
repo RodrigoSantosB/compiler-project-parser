b15b1cb4482c31f4941a3c06fe6da689678e54a0
Enzo Bissoli <enzogurgel15@gmail.com>
2026-03-26 03:52:36 -0300

    feat: C lexer and increased semantic understanding

--- paper-examples/src/CParser.hs
+++ paper-examples/src/CParser.hs
@@ -2,15 +2,16 @@ module CParser (readSrc) where
 
 import Prelude (IO, FilePath, String, (>>=), return, print, putStrLn, snd, ($))
 
-import Prelude (readFile, Eq, Either)
+import Prelude (readFile, Eq, Either, Bool(True, False))
 
 -- type Parser = (Parsec String () Identity), needs to also pass the return type
 -- type ParsecT = Stream type(input), user state(optional), monad and then return type
 -- the original signature Parser a a :: input output -> [([input],output)] would be
 -- ParsecT input () Identity output 
 import Text.Parsec.String (Parser, parseFromFile)
-import Text.Parsec (ParseError, letter, parse, many)
+import Text.Parsec ((<|>))
 
+import qualified Text.Parsec as Parsec
 import qualified Text.Parsec.Token as Token
 import qualified Text.Parsec.Char as Char
 import qualified Text.Parsec.Combinator as Combinator
@@ -32,20 +33,36 @@ import qualified Text.Parsec.Expr as Expr
 -- this reads the src code for our toy c code
 -- for the moment we'll hardcode the path
 
-readSrc :: FilePath -> IO (Either ParseError String)
+readSrc :: FilePath -> IO (Either Error.ParseError String)
 readSrc _ =  readFile "./src/toy.c" >>= \content ->
-		    return $ parse (many letter) "./src/toy.c" content
+		    return $ Prim.parse (Prim.many Char.letter) "./src/toy.c" content
 
-CLang = Token.LanguageDef {
-	Token.commentStart = "/*",
-	Token.commentEnd = "*/",
-	Token.commentLine = "//",
-	Token.identStart = letter <|>'_', -- identifier can start with _,
-	Token.identLetter = letter <|> '_', -- identifier can end with _,
-	Token.opStart     = oneOf "+-*/%=&|<>!"
-}
+ctokens :: Token.LanguageDef ()
+ctokens = Token.LanguageDef {
+    Token.commentStart = "/*",
+    Token.commentEnd = "*/",
+    Token.commentLine = "//",
+    Token.nestedComments = False,           
+    Token.identStart = Char.letter <|> Char.char '_',
+    Token.identLetter = Char.alphaNum <|> Char.char '_',
+    Token.opStart = Char.oneOf "+-*/%=&|<>!",
+    Token.opLetter = Char.oneOf "+-*/%=&|<>!",
+    Token.reservedNames = [
+        "auto", "break", "case", "char", "const", "continue", "default",
+        "do", "double", "else", "enum", "extern", "float", "for",
+        "goto", "if", "int", "long", "register", "return", "short",
+        "signed", "sizeof", "static", "struct", "switch", "typedef",
+        "union", "unsigned", "void", "volatile", "while"
+        ],
+    Token.reservedOpNames = [              
+        "+", "-", "*", "/", "%", "=", "==", "!=", "<", ">", "<=", ">=",
+        "&&", "||", "!", "&", "|", "^", "~", "<<", ">>", "++", "--",
+        "+=", "-=", "*=", "/=", "%=", "&=", "|=", "^=", "<<=", ">>="
+        ],
+    Token.caseSensitive = True
+    }
 
-readString :: Either ParseError Identifier
+clexer = Token.makeTokenParser ctokens
 
 -- Now we need to read strings and deal with any problems that might appear
 
