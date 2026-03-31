c10d3730f268b9408a65246afb748eb36b94b65a
Enzo Bissoli <enzogurgel15@gmail.com>
2026-03-31 09:42:28 -0300

    feat: wip

--- paper-examples/src/CParserv5.hs
+++ paper-examples/src/CParserv5.hs
@@ -7,7 +7,7 @@ import Data.Maybe (catMaybes)
 import Control.Applicative ((<*>),(<*),(<$),(*>))
 
 import Text.Parsec.String (Parser, parseFromFile)
-import Text.Parsec ((<|>),ParsecT)
+import Text.Parsec ((<|>),(<?>),ParsecT)
 
 import qualified Text.Parsec as Parsec
 import qualified Text.Parsec.Token as Token
@@ -19,25 +19,29 @@ import qualified Text.Parsec.Pos as Pos
 import qualified Text.Parsec.Expr as Expr
 
 -- algebraic data types
+data TopLevel
+    = VarDecl Type String (Maybe Expr)
+    | FuncDecl Type String [Parameter] Block
+    deriving Show
+
 data Expr 
     = Number String                    
     | Var String                       
     | Assign Expr Expr               
     | Call String [Expr]               
-    | Function Type String [Parameter] Block
     deriving Show
 
 data Stmt
-     =  Decl Type Expr (Maybe Expr)    
+     = Decl Type String (Maybe Expr)    
      | ExprStmt Expr
-     | Return (maybe Expr)
-     | Block [Stmt]
+     | Return (Maybe Expr)
+     | BlockStmt Block
      deriving Show
 
 data Parameter = Parameter Type String
     deriving Show
 
-type Block = [Expr]
+type Block = [Stmt]
 
 data Type 
     = CharType
@@ -81,6 +85,7 @@ parseNum =  Token.naturalOrFloat clexer >>= \read ->
 		case read of
 			Left n -> return $ Number (show n)
 			Right n -> return $ Number (show n)
+
 -- same as Token.identifier clexer >>= \exp -> Parser Var exp
 parseVar :: Parser Expr
 parseVar = Prim.parsecMap Var (Token.identifier clexer)
@@ -98,21 +103,19 @@ parseAssign = Assign
 	     <$>
 	     (parseVar <* Token.reservedOp clexer "=") 
 	     <*>
-	     (parseNum <* Token.semi clexer)
+	     (parseNum)
 
 parseDecl :: Parser Stmt
 parseDecl = Decl
             <$> 
 	    (parseType)                
             <*> 
-	    (parseVar)                
+	    (parseIdentifier)                
             <*> 
-	    (Combinator.optionMaybe (Token.reservedOp clexer "=" *> parseCExpr) <* Token.semi clexer)
+	    (Combinator.optionMaybe (Token.reservedOp clexer "=" *> parseCExpr))
 
 parseIdentifier :: Parser String
 parseIdentifier = Token.identifier clexer
-
-
 
 parseParameter :: Parser Parameter
 parseParameter = Parameter
@@ -120,16 +123,6 @@ parseParameter = Parameter
 		  (parseType)
 		  <*>
 		  (Token.identifier clexer)
-	
-parseFunc :: Parser Expr
-parseFunc = Function
-	    <$>
-	    (parseType)
-	    <*>
-	    (parseIdentifier)
-	    <*>
-	    (Char.char '(' *> many parseParameter <* Char.char ')')
-
 
 parseCall :: Parser Expr
 parseCall = Call
@@ -146,6 +139,8 @@ parseCExpr :: ParsecT String () Identity Expr
 parseCExpr = parseAssign 
 	     <|> 
 	     parseVar 
+	     <|>
+	     parseCall
 	     <|> 
 	     parseNum
 
@@ -156,16 +151,45 @@ parseCExprStmt = ExprStmt
 
 -- statement parser
 parseCStmt :: ParsecT String () Identity Stmt
-parseCStmt = Prim.try parseDecl
+parseCStmt = Prim.try (parseDecl <* Token.semi clexer)
+	     <|>
+	     Prim.try parseBlock
 	     <|>
 	     Prim.try parseCExprStmt 
 
+parseBlock :: Parser Stmt
+parseBlock = BlockStmt
+	     <$>
+	     (Char.char '{' *> Prim.many parseCStmt <* Char.char '}')
+
+parseFunc :: Parser TopLevel
+parseFunc = FuncDecl
+	    <$>
+	    (parseType)
+	    <*>
+	    (parseIdentifier)
+	    <*>
+	    (Char.char '(' *> Prim.many parseParameter <* Char.char ')')
+	    <*>
+	    (parseBlock)
+
+parseTopVar :: Parser TopLevel
+parseTopVar = VarDecl
+	<$> parseType
+	<*> parseIdentifier
+	<*> Combinator.optionMaybe (Token.reservedOp clexer "=" *> parseCExpr)
+
+parseTopDecl :: Parser TopLevel
+parseTopDecl = (Prim.try parseTopVar <* Token.semi clexer)
+		<|> (Prim.try parseFunc <* Token.semi clexer)
+		<?> "top-level declaration"
+
 -- parse all src code
-parseTopLevel :: ParsecT String () Identity [Stmt]
-parseTopLevel = Prim.many  parseCStmt
+parseTopLevel :: ParsecT String () Identity [TopLevel]
+parseTopLevel = Prim.many  parseTopDecl
 
 -- parser entrypoint
-parseSrc :: FilePath -> IO (Either Error.ParseError [Stmt])
+parseSrc :: FilePath -> IO (Either Error.ParseError [TopLevel])
 parseSrc filepath =  readFile filepath >>= \content ->
 		    return $ Prim.parse (parseTopLevel) filepath content
 
